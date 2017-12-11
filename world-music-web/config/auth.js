63277755cc59f81d560a877cbc93e579450b5917
chrisf <f010243@126.com>
2017-12-11 17:21:10 -0500

    clear some code

--- world-music-web/config/auth.js
+++ world-music-web/config/auth.js
@@ -1,15 +1,12 @@
-// config/auth.js
 
-// expose our config directly to our application using module.exports
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'        : '150159565750193', // your App ID
-        'clientSecret'    : '88805a61ff1f9a7f10f665e6e8bd9c0b', // your App Secret
+        'clientID'        : '150159565750193', 
+        'clientSecret'    : '88805a61ff1f9a7f10f665e6e8bd9c0b', 
         'callbackURL'     : 'http://music.alanzyy.com/auth/facebook/callback',
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
-        'profileFields'   : ['id', 'email', 'name'] // For requesting permissions from Facebook API
-
+        'profileFields'   : ['id', 'email', 'name'] 
     }
 
 };
